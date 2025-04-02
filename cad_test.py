59efb696478b9b5e286b0c0d1e949191ee16ebef
fr4nt1x <fr4nt1x@debianmuc.de>
2025-04-02 20:23:46 +0200

    Use all lines for construction

--- cad_test.py
+++ cad_test.py
@@ -7,12 +7,13 @@ from hexplant import HexPlant
 set_port(3939)
 
 set_defaults(reset_camera=Camera.CENTER, helper_scale=5)
+show_clear()
 import random
 
-nx = 16
-ny = 16
+nx = 32
+ny = 32
 hex = HexPlant(nx, ny)
-number_lines = 10
+number_lines = 5
 lines = []
 for i in range(0, number_lines + 1):
     x = random.randint(0, nx / 4)
@@ -25,37 +26,40 @@ line_converted = [
     hex.convert_point_to_hexagonal(point[0], point[1], 100) for point in line
 ]
 # line_converted = [(10, 0), (10, 10), (0, 10)]
-print(line_converted)
-# for line in lines:
-#     line_converted = [
-#         hex.convert_point_to_hexagonal(point[0], point[1]) for point in line
-#     ]
 
-with BuildPart() as ex1:
-    with BuildLine() as ex1_ln:
-        l = Polyline(line_converted)
-    z_dir = (
-        line_converted[1][0] - line_converted[0][0],
-        line_converted[1][1] - line_converted[0][1],
-    )
-    plane = Plane(origin=line_converted[0], z_dir=z_dir)
-    with BuildSketch(plane) as rectangleSK:
-        Circle(2)
-    sweep(transition=Transition.ROUND)
-show_all()
+all_lines = []
+all_circles = []
+with BuildPart() as branches:
+    for line in lines:
+        line_converted = [
+            hex.convert_point_to_hexagonal(point[0], point[1], 100) for point in line
+        ]
+        # print(line_converted)
+        with BuildLine() as ex1_ln:
+            l = Polyline(line_converted)
+        all_lines += ex1_ln.line
 
-# %%
+        z_dir = (
+            line_converted[1][0] - line_converted[0][0],
+            line_converted[1][1] - line_converted[0][1],
+        )
 
-a, b = 40, 20
+        plane = Plane(origin=line_converted[0], z_dir=l % 0)
 
-with BuildPart() as ex14:
-    with BuildLine() as ex14_ln:
-        l1 = JernArc(start=(0, 0), tangent=(0, 1), radius=a, arc_size=180)
-        l2 = JernArc(start=l1 @ 1, tangent=l1 % 1, radius=a, arc_size=-90)
-        l3 = Line(l2 @ 1, l2 @ 1 + (-a, a))
-    with BuildSketch(Plane.XZ) as ex14_sk:
-        Rectangle(b, b)
-    sweep()
+        with BuildSketch(plane) as rectangleSK:
+            Circle(1)
+        all_circles += branches.pending_faces
+        sweep(transition=Transition.ROUND)
 
-show_all()
+show_object(all_lines)
+show_object(all_circles)
+# for i, line in enumerate(all_lines):
+#     show_object(line, name="line" + str(i))
+
+# for i, circle in enumerate(all_circles):
+#     show_object(circle, name="circle" + str(i))
+show_object(branches.part)
+# show_all()
+
+
 # %%
